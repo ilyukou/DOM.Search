e27bec172327b2103130fafca1496ce843f64972
Ilya Ilyukou <ilyailyukou@gmail.com>
2018-05-08 19:47:22 +0200

    Bootstrap test

--- script/Algoritm.js
+++ script/Algoritm.js
@@ -1,26 +1,28 @@
-function search(tagname) {
+function search() {
 
     var strFind = document.getElementById("strFind").value;
-    var divInfo = document.getElementById('htmInfo');
+    var divInfo = document.getElementById('htmlInfo');
 
     var o = document.getElementById( 'search' );
     for( var i = 0; i < o.length; i++ ) {
         if( o[i].checked == '1' ) {
 
-            if(o[i].value == 'tag'){
+            if(o[i].value == undefined){
+                break
+            }else if(o[i].value == 'tag'){
 
                 var elem = divInfo.getElementsByTagName(strFind);
                 var divInfoTag = document.getElementById('outputInfo');
 
-            }
-            if(o[ i ].value == 'class'){
+            }else if(o[ i ].value == 'class'){
                 var elem = divInfo.getElementsByClassName(strFind);
                 var divInfoTag = document.getElementById('outputInfo');
             }
-            if(o[ i ].value == 'id'){
+            /*if(o[ i ].value == 'id'){
                 var elem = divInfo.getElementById(strFind);
                 var divInfoTag = document.getElementById('outputInfo');
-            }
+            }*/
+
             for(var i=0; i<elem.length; i++){
                 var input = elem[i];
                 var textStrFind = document.createElement('p');
--- script/changeInput.js
+++ script/changeInput.js
@@ -2,7 +2,7 @@ document.getElementById("openFile").addEventListener('change',function () {
 
     var fr = new FileReader();
     fr.onload = function () {
-        document.getElementById("htmInfo").innerHTML=this.result;};
+        document.getElementById("htmlInfo").innerHTML=this.result;};
     fr.readAsText(this.files[0]);});
 
 
